284a3f8ec4581c9537441fded182a4963baed819
Humberto Rocha <humrochagf@gmail.com>
2015-10-22 17:59:07 -0200

    add api_test resource

--- tapioca_slack/resource_mapping.py
+++ tapioca_slack/resource_mapping.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
 RESOURCE_MAPPING = {
-    'resource_name': {
-        'resource': 'resource_url',
-        'docs': 'http://www.documentation.com/link/to/resource/documentation'
+    'api_test': {
+        'resource': 'api.test',
+        'docs': 'https://api.slack.com/methods/api.test'
     },
 }
