a26b09c5a79b790d99b2bd1c9c9635d8ae9cd821
Humberto Rocha <humrochagf@gmail.com>
2015-10-22 22:19:46 -0200

    add part of the resources

--- tapioca_slack/resource_mapping.py
+++ tapioca_slack/resource_mapping.py
@@ -1,8 +1,191 @@
 # coding: utf-8
 
 RESOURCE_MAPPING = {
+    # api
     'api_test': {
         'resource': 'api.test',
         'docs': 'https://api.slack.com/methods/api.test'
+    },
+
+    # auth
+    'auth_test': {
+        'resource': 'auth.test',
+        'docs': 'https://api.slack.com/methods/auth.test'
+    },
+
+    # channels
+    'channels_archive': {
+        'resource': 'channels.archive',
+        'docs': 'https://api.slack.com/methods/channels.archive'
+    },
+    'channels_create': {
+        'resource': 'channels.create',
+        'docs': 'https://api.slack.com/methods/channels.create'
+    },
+    'channels_history': {
+        'resource': 'channels.history',
+        'docs': 'https://api.slack.com/methods/channels.history'
+    },
+    'channels_info': {
+        'resource': 'channels.info',
+        'docs': 'https://api.slack.com/methods/channels.info'
+    },
+    'channels_kick': {
+        'resource': 'channels.kick',
+        'docs': 'https://api.slack.com/methods/channels.kick'
+    },
+    'channels_leave': {
+        'resource': 'channels.leave',
+        'docs': 'https://api.slack.com/methods/channels.leave'
+    },
+    'channels_list': {
+        'resource': 'channels.list',
+        'docs': 'https://api.slack.com/methods/channels.list'
+    },
+    'channels_mark': {
+        'resource': 'channels.mark',
+        'docs': 'https://api.slack.com/methods/channels.mark'
+    },
+    'channels_rename': {
+        'resource': 'channels.rename',
+        'docs': 'https://api.slack.com/methods/channels.rename'
+    },
+    'channels_set_purpose': {
+        'resource': 'channels.setPurpose',
+        'docs': 'https://api.slack.com/methods/channels.setPurpose'
+    },
+    'channels_set_topic': {
+        'resource': 'channels.setTopic',
+        'docs': 'https://api.slack.com/methods/channels.setTopic'
+    },
+    'channels_unarchive': {
+        'resource': 'channels.unarchive',
+        'docs': 'https://api.slack.com/methods/channels.unarchive'
+    },
+
+    # chat
+    'chat_delete': {
+        'resource': 'chat.delete',
+        'docs': 'https://api.slack.com/methods/chat.delete'
+    },
+    'chat_post_message': {
+        'resource': 'chat.postMessage',
+        'docs': 'https://api.slack.com/methods/chat.postMessage'
+    },
+    'chat_update': {
+        'resource': 'chat.update',
+        'docs': 'https://api.slack.com/methods/chat.update'
+    },
+
+    # emoji
+    'emoji_list': {
+        'resource': 'emoji.list',
+        'docs': 'https://api.slack.com/methods/emoji.list'
+    },
+
+    # files
+    'files_delete': {
+        'resource': 'files.delete',
+        'docs': 'https://api.slack.com/methods/files.delete'
+    },
+    'files_info': {
+        'resource': 'files.info',
+        'docs': 'https://api.slack.com/methods/files.info'
+    },
+    'files_list': {
+        'resource': 'files.list',
+        'docs': 'https://api.slack.com/methods/files.list'
+    },
+    'files_upload': {
+        'resource': 'files.upload',
+        'docs': 'https://api.slack.com/methods/files.upload'
+    },
+
+    # groups
+    'groups_archive': {
+        'resource': 'groups.archive',
+        'docs': 'https://api.slack.com/methods/groups.archive'
+    },
+    'groups_close': {
+        'resource': 'groups.close',
+        'docs': 'https://api.slack.com/methods/groups.close'
+    },
+    'groups_create': {
+        'resource': 'groups.create',
+        'docs': 'https://api.slack.com/methods/groups.create'
+    },
+    'groups_create_child': {
+        'resource': 'groups.createChild',
+        'docs': 'https://api.slack.com/methods/groups.createChild'
+    },
+    'groups_history': {
+        'resource': 'groups.history',
+        'docs': 'https://api.slack.com/methods/groups.history'
+    },
+    'groups_info': {
+        'resource': 'groups.info',
+        'docs': 'https://api.slack.com/methods/groups.info'
+    },
+    'groups_invite': {
+        'resource': 'groups.invite',
+        'docs': 'https://api.slack.com/methods/groups.invite'
+    },
+    'groups_kick': {
+        'resource': 'groups.kick',
+        'docs': 'https://api.slack.com/methods/groups.kick'
+    },
+    'groups_leave': {
+        'resource': 'groups.leave',
+        'docs': 'https://api.slack.com/methods/groups.leave'
+    },
+    'groups_list': {
+        'resource': 'groups.list',
+        'docs': 'https://api.slack.com/methods/groups.list'
+    },
+    'groups_mark': {
+        'resource': 'groups.mark',
+        'docs': 'https://api.slack.com/methods/groups.mark'
+    },
+    'groups_open': {
+        'resource': 'groups.open',
+        'docs': 'https://api.slack.com/methods/groups.open'
+    },
+    'groups_rename': {
+        'resource': 'groups.rename',
+        'docs': 'https://api.slack.com/methods/groups.rename'
+    },
+    'groups_set_purpose': {
+        'resource': 'groups.setPurpose',
+        'docs': 'https://api.slack.com/methods/groups.setPurpose'
+    },
+    'groups_set_topic': {
+        'resource': 'groups.setTopic',
+        'docs': 'https://api.slack.com/methods/groups.setTopic'
+    },
+    'groups_unarchive': {
+        'resource': 'groups.unarchive',
+        'docs': 'https://api.slack.com/methods/groups.unarchive'
+    },
+
+    # im
+    'im_close': {
+        'resource': 'im.close',
+        'docs': 'https://api.slack.com/methods/im.close'
+    },
+    'im_history': {
+        'resource': 'im.history',
+        'docs': 'https://api.slack.com/methods/im.history'
+    },
+    'im_list': {
+        'resource': 'im.list',
+        'docs': 'https://api.slack.com/methods/im.list'
+    },
+    'im_mark': {
+        'resource': 'im.mark',
+        'docs': 'https://api.slack.com/methods/im.mark'
+    },
+    'im_open': {
+        'resource': 'im.open',
+        'docs': 'https://api.slack.com/methods/im.open'
     },
 }
